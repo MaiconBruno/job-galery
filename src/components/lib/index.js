899cff919c0afabd1f41634aa9f42d4e5dc55ddf
Maicon Bruno <maiconbrunorodrigues@gmail.com>
2021-03-02 15:47:40 -0300

    .

--- src/components/lib/index.js
+++ src/components/lib/index.js
@@ -1,23 +1,26 @@
 
-import { Body, Container, Card, SubContainer } from './styles';
+import { Body, Container, Card, SubContainer, Title } from './styles';
 export default function Library() {
        
     var projects = [
         {
             banner:'https://educacaoinfantil.aix.com.br/wp-content/uploads/2018/03/177959-desenvolvimento-cognitivo-9-praticas-para-a-sala-de-aula.jpg',
-            title:'My First Project',
+            company:'Mcb',
+            title:'Mobile APP',
             description:'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             images:[],
         },
         {
-            banner:'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.06274509803921569&fundo_g=0.996078431372549&fundo_b=0.9568627450980393&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10',
-            title:'My Second Project',
+            banner:'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.16593016684055328&fundo_g=0.2154541015625&fundo_b=0.21337326396914089&texto_r=0&texto_g=0&texto_b=0&texto=&tamanho_fonte=10',
+            company:'Rei do tebori',
+            title:'Mobile App',
             description:'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             images:[],
         },
         {
-            banner:'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.16593016684055328&fundo_g=0.2154541015625&fundo_b=0.21337326396914089&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10',
-            title:'My third Project',
+            banner:'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.730712890625&fundo_g=1&fundo_b=0&texto_r=0&texto_g=0&texto_b=0&texto=&tamanho_fonte=10',
+            company:'Loja do Desejo',
+            title:'Website',
             description:'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             images:[],
         }
@@ -29,8 +32,7 @@ export default function Library() {
                 <SubContainer>
                     {projects.map(data =>(
                         <Card image={data.banner}>
-                            <Title>{data.title}</Title>
-                            <Description>{data.description}</Description>
+                            <Title>{data.company}</Title>
                         </Card>      
                     ))}                       
                 </SubContainer>
--- src/components/lib/styles.js
+++ src/components/lib/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+
+// Start - Body Config
 export const Body = styled.div`
     display:flex;
     align-items:center;
@@ -8,7 +10,9 @@ export const Body = styled.div`
     width:100%;
     background-color:white;
 `;
+// End - Body Config
 
+// Start - Containter cards 
 export const Container = styled.div`
     width:100%;
     height:auto;
@@ -29,14 +33,22 @@ export const SubContainer = styled.div`
     background-color:#f3f3f3;
     
 `;
+// End - Containter Cards
 
+// Start - Card Intes 
 export const Card = styled.div`
     max-width:23%;
     width:23%;
     margin:1%;
     min-height:200px;
+    display:flex;
+    align-items:flex-end;
+    justify-content:center;
     background-image:url(${props => props.image});
-    
+    border-radius:15px;
+    box-shadow: 5px 5px 0px 0px rgba(25,25,25, 0.1);
+    cursor:cell;
+
     @media(min-width:2520px){
         width:18%;
         max-width:100%;
@@ -48,4 +60,17 @@ export const Card = styled.div`
     }
 
 `;
+
+export const Title = styled.h2`
+    color:white;
+    text-transform:uppercase;
+    font-size:18px;
+    cursor:default;
+`;
+
+export const Description = styled.p`
+
+`;
+// End - Card Intes
+
 
